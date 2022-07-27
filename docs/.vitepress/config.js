081de8c9e33907d6a0146bfb9d8bacb7ec5d3794
dreampasssser <dreampasssser@foxmail.com>
2022-07-27 15:54:30 +0800

    sth

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,24 @@
+const nav = [
+  {
+    text: 'Vue',
+    link: '/Vue/'
+  },
+  {
+    text: 'React',
+    link: '/React/'
+  },
+  {
+    text: '算法',
+    link: '/算法/'
+  }
+]
+
 export default {
-  title: 'VitePress',
-  description: 'Just playing around.'
+  lang: 'zh-CN',
+  title: "Dreampasssser's Blog",
+  description: 'A VitePress site',
+  themeConfig: {
+    nav,
+    sidebar: 'auto'
+  }
 }

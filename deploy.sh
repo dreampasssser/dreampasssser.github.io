8c55c1547fbd2c0b9bf78f99276c3dfee1a2e975
dreampasssser <dreampasssser@foxmail.com>
2022-07-27 17:47:17 +0800

    修改配置。

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f https://github.com/dreampasssser/dreampasssser.github.io.git main
+git push -f https://github.com/dreampasssser/dreampasssser.github.io.git main:gh-pages
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
